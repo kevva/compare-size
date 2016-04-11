3a30d8d691d0b02904c6551cd2f079cf6c1005e1
Kevin Martensson <kevinmartensson@gmail.com>
2016-04-11 17:06:30 +0200

    Bump AVA

--- test/test.js
+++ test/test.js
@@ -1,29 +1,22 @@
-'use strict';
-var path = require('path');
-var test = require('ava');
-var compareSize = require('../');
+import path from 'path';
+import test from 'ava';
+import fn from '../';
 
-test('compare size between two files', function (t) {
-	t.plan(3);
+const a = path.join(__dirname, 'fixtures', 'test-a.txt');
+const b = path.join(__dirname, 'fixtures', 'test-b.txt');
 
-	var a = path.join(__dirname, 'fixtures', 'test-a.txt');
-	var b = path.join(__dirname, 'fixtures', 'test-b.txt');
+test('compare size between two files', async t => {
+	const data = await fn(a, b);
 
-	compareSize(a, b).then(function (res) {
-		t.assert(res[a] === 9, res[a]);
-		t.assert(res[b] === 7, res[b]);
-		t.assert(res.difference === 2, res.difference);
-	});
+	t.is(data[a], 9);
+	t.is(data[b], 7);
+	t.is(data.difference, 2);
 });
 
-test('synchronously compare size between two files', function (t) {
-	t.plan(3);
+test('synchronously compare size between two files', t => {
+	const data = fn.sync(a, b);
 
-	var a = path.join(__dirname, 'fixtures', 'test-a.txt');
-	var b = path.join(__dirname, 'fixtures', 'test-b.txt');
-	var res = compareSize.sync(a, b);
-
-	t.assert(res[a] === 9, res[a]);
-	t.assert(res[b] === 7, res[b]);
-	t.assert(res.difference === 2, res.difference);
+	t.is(data[a], 9);
+	t.is(data[b], 7);
+	t.is(data.difference, 2);
 });
